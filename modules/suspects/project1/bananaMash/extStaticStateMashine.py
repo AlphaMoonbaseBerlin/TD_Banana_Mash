a9279da9af2b7c062688045faaa50e54cd799823
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2023-09-08 17:52:49 +0200

    Version 2! Now with "Stores2 and more arguments. And new PI

--- modules/suspects/project1/bananaMash/extStaticStateMashine.py
+++ modules/suspects/project1/bananaMash/extStaticStateMashine.py
@@ -1,10 +1,8 @@
+
 '''Info Header Start
 Name : extStaticStateMashine
 Author : Wieland@AMB-ZEPH15
-Version : 0
-Build : 2
-Savetimestamp : 2023-07-24T22:33:17.832315
-Saveorigin : staticStateMashine.14.toe
+Saveorigin : bananaMash.toe
 Saveversion : 2022.28040
 Info Header End'''
 from sqlite3 import connect
@@ -21,91 +19,112 @@ class extStaticStateMashine:
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
 
-		# properties
-		TDF.createProperty(self, 	'Current_State', 
-									value = self.ownerComp.par.Defaultstate.eval() , 
-									dependable=True,
-						   			readOnly=False )
-		self.state_timer = self.ownerComp.op('stateTimer')
+		
+		self.CurrentState = tdu.Dependency( self.ownerComp.par.Defaultstate.eval() )
+
+		self.stateTimer = self.ownerComp.op('stateTimer')
 		self.ownerComp.op("Emitter").Attach_Emitter(self)
-		self.GoToState = self.Go_To_State
+		self.GoToState = self.GoToState
 		self.callback = self.ownerComp.op('callbackManager')
-		self.Check = self.Update_State
-		self.states = self.Get_States_Dict()
-		if self.ownerComp.par.Autocontinueonstateenter.eval(): self.Update_State()		
+		self.Check = self.UpdateState
+		self.states = self.GetStatesDict()
+		if self.ownerComp.par.Autocontinueonstateenter.eval(): self.UpdateState()		
 
 	def Do_Callback(self, name, *args, **kwargs):
 		self.callback.Do_Callback( f"On{name}", *args, **kwargs)
 		self.Emit( name, *args, **kwargs)
 
 	@property
-	def stateholder(self):
+	def States(self):
 		return self.ownerComp.op("state_repo_maker").Repo
 
-	def Get_States_Dict( self ):
-		return { state.name : self.parse_state(state) for state in self.stateholder.findChildren(depth = 1) }
-		
-	def parse_state(self, state):
-		return { connection.name : self.read_connection(connection) for connection in state.findChildren(depth = 1) }
+	@property
+	def Store(self):
+		return self.ownerComp.op("repo_maker").Repo
 
-	def Update_Connection(self, connection):
-		self.states[ connection.parent().name ][ connection.name ] = self.read_connection(connection)
+	def GetStatesDict( self ):
+		return { state.name : self.parseState(state) for state in self.States.findChildren(depth = 1) }
+		
+	def parseState(self, state):
+		return { connection.name : self.readConnection(connection) for connection in state.findChildren(depth = 1) }
+
+	def UpdateConnection(self, connection):
+		self.states[ connection.parent().name ][ connection.name ] = self.readConnection(connection)
 	
-	def read_connection(self, connection):
+	def readConnection(self, connection):
 		return connection
 
-	def evaluate_cell(self, value):
-		try:
-			return eval( value )
-		except:
-			return value
 
+	def executeStatechangeCallbacks(self, newState):
+		self.Do_Callback( "StateExit", 
+				   self.CurrentState.val, 
+				   newState,
+				   self.ownerComp,
+				   self.Store)
+		self.Do_Callback( "StateEnter", 
+				   self.CurrentState.val, 
+				   newState, 
+				   self.GetStateTime( newState ),
+				   self.ownerComp,
+				   self.Store )
 
-	def execute_statechange_callbacks(self, new_state):
-		self.Do_Callback( "StateExit", self.Current_State, new_state)
-		self.Do_Callback( "StateEnter", self.Current_State, new_state, self.Get_State_Time( new_state ) )
+		self.Do_Callback( f"{self.CurrentState.val}Exit", 
+				   self.CurrentState.val, 
+				   newState,
+				   self.ownerComp,
+				   self.Store )
+		self.Do_Callback( f"{newState}Enter", 
+				   self.CurrentState.val, 
+				   newState, self.GetStateTime( newState ),
+				   self.ownerComp,
+				   self.Store )
 
-		self.Do_Callback( f"{self.Current_State}Exit", self.Current_State, new_state )
-		self.Do_Callback( f"{new_state}Enter", self.Current_State, new_state, self.Get_State_Time( new_state ) )
+	def executeStatecycleCallbacks(self):
+		self.Do_Callback( "StateCycle", 
+				   self.CurrentState.val, 
+				   self.GetStateTime( self.CurrentState.val),
+				   self.ownerComp,
+				   self.Store )
+		self.Do_Callback( f"{self.CurrentState.val}Cycle", 
+				   self.CurrentState.val,
+				   self.GetStateTime( self.CurrentState.val),
+				   self.ownerComp,
+				   self.Store )
 
-	def execute_statecycle_callback(self):
-		self.Do_Callback( "StateCycle", self.Current_State, self.Get_State_Time( self.Current_State) )
-		self.Do_Callback( f"{self.Current_State}Cycle", self.Get_State_Time( self.Current_State) )
+	def GetStateTime(self, state):
+		return self.States.op(state).par.Length.eval() if self.States.op(state) else 0
 
-	def Get_State_Time(self, state):
-		return self.stateholder.op(state).par.Length.eval() if self.stateholder.op(state) else 0
+	def startTimer(self, state):
+		self.stateTimer.par.length = self.GetStateTime ( state )
+		self.stateTimer.par.start.pulse()
 
-	def start_timer(self, state):
-		self.state_timer.par.length = self.Get_State_Time ( state )
-		self.state_timer.par.start.pulse()
+	def ResetTimer(self):
+		self.startTimer( self.CurrentState.val )
 
-	def Reset_Timer(self):
-		self.start_timer( self.Current_State )
-
-	def Go_To_State(self, new_state):
-		if self.Current_State == new_state: return self.Reset_Timer()
-		self.execute_statechange_callbacks( new_state )
-		self.Current_State = new_state
-		if self.ownerComp.par.Timeactive.eval(): return self.start_timer( new_state )
-		if self.ownerComp.par.Autocontinueonstateenter.eval(): self.Update_State()
+	def GoToState(self, newState):
+		if self.CurrentState.val == newState: return self.ResetTimer()
+		self.executeStatechangeCallbacks( newState )
+		self.CurrentState.val = newState
+		if self.ownerComp.par.Timeactive.eval(): return self.startTimer( newState )
+		if self.ownerComp.par.Autocontinueonstateenter.eval(): self.UpdateState()
 		return
 
-	def Update_State( self ):
+	def UpdateState( self ):
 		new_state = self.Check_Conditions()
-		if not new_state: return self.execute_statecycle_callback()
+		if not new_state: return self.executeStatecycleCallbacks()
 		self.GoToState(new_state)
 		return new_state
 
-	def New_State(self, name):
-		new_state = self.stateholder.copy( self.ownerComp.op('statetemplate'), name = name )
-		self.states[new_state.name] = self.parse_state( new_state )
+	def NewState(self, name):
+		new_state = self.States.copy( self.ownerComp.op('statetemplate'), name = name )
+		self.states[new_state.name] = self.parseState( new_state )
 		return new_state
 		
-	def Print_States(self):
+	def PrintStates(self):
 		debug( self.states )
 		
-	def Delete_State(self, name):
-		for deleteme in self.stateholder.findChildren( name = name, maxDepth = 3):
+	def DeleteState(self, name):
+		for deleteme in self.States.findChildren( name = name, maxDepth = 3):
 			try:
 				del self.states[ deleteme.parent().name ][ deleteme.name ]
 			except:
@@ -113,26 +132,24 @@ class extStaticStateMashine:
 			deleteme.destroy()
 		del self.states[name]
 
-	def New_Connection(self, start_state, to_state):
-		if self.stateholder.op(start_state).op(to_state): return
+	def NewConnection(self, start_state, to_state):
+		if self.States.op(start_state).op(to_state): return
 
-		new_connection = self.stateholder.op(start_state).copy(self.ownerComp.op("connection_callback"), name = to_state)
+		new_connection = self.States.op(start_state).copy(self.ownerComp.op("connection_callback"), name = to_state)
 		self.states[start_state][to_state] = new_connection
 	
-	def Delete_Connection( self, state, target):
-		self.stateholder.op(state).op(target).destroy()
+	def DeleteConnection( self, state, target):
+		self.States.op(state).op(target).destroy()
 		del self.states[state][target]
 		return
 
-	@property
-	def condition_comp(self):
-		return self.ownerComp.op("repo_maker").Repo
+	
 
 	def Check_Conditions( self ):
-		if self.Current_State not in self.states: return None
-		for connection_key in self.states[ self.Current_State ]:
-			check_function = getattr( self.states[ self.Current_State ][ connection_key ].module , "check", false_callback)
+		if self.CurrentState.val not in self.states: return None
+		for connection_key in self.states[ self.CurrentState.val ]:
+			check_function = getattr( self.states[ self.CurrentState.val ][ connection_key ].module , "check", false_callback)
 		
-			if not check_function( self.Current_State, connection_key, self.condition_comp ): continue
+			if not check_function( self.CurrentState.val, connection_key, self.Store ): continue
 			return connection_key
 
---
+++ modules/suspects/project1/bananaMash/ui/extStateUI.py
@@ -0,0 +1,83 @@
+'''Info Header Start
+Name : extStateUI
+Author : Wieland@AMB-ZEPH15
+Saveorigin : bananaMash.toe
+Saveversion : 2022.28040
+Info Header End'''
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+
+class extStateUI:
+
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+
+		# properties
+		TDF.createProperty(self, 'SelectedState', value='', dependable=True,
+						   readOnly=False)
+
+		TDF.createProperty(self, 'NextConnection', value='', dependable=True,
+						   readOnly=False)
+
+		TDF.createProperty(self, 'SelectedConnection', value='', dependable=True,
+						   readOnly=False)
+	@property 
+	def Statemashine(self):
+		return self.ownerComp.par.Statemashine.eval()
+
+	def Select_State(self, component):
+		
+		self.SelectedState = component.name
+
+	def Reset_Selected_State(self):
+		self.SelectedState = ''
+
+	def Delete_Selected_State(self):
+		self.Statemashine.Delete_State( self.SelectedState )
+		self.Reset_Selected_State()
+
+	def New_State(self):
+		self.ownerComp.op('new_state_prompt').Open()
+		return
+	
+	def _New_State(self, name):
+		self.SelectedState = self.Statemashine.New_State( name ).name
+
+	def Go_To_Selected(self):
+		self.Statemashine.GoTo( self.SelectState )
+
+
+	def Delete_Connection(self, path):
+		self.Statemashine.Delete_Connection( path.split("/")[-2], path.split("/")[-1])
+
+	def Select_Connection(self, path):
+		
+		self.SelectedConnection = op(path)
+
+	def Interact_Connection(self, component):
+		if self.NextConnection == component.name: return
+		if self.NextConnection:
+			self.Statemashine.New_Connection( self.NextConnection,
+											  component.name ) 
+			self.Reset_Next_Connection()
+			return
+		 
+		self.NextConnection = component.name
+		return
+
+	def Reset_Next_Connection(self):
+		self.NextConnection = ''
+		return
+
+	def Add_Condition(self):
+		path = self.SelectedConnection.path
+		self.Statemashine.Add_Condition( path.split("/")[-2], path.split("/")[-1])
+
+	def Delete_Condition(self, name):
+		path = self.SelectedConnection.path
+		self.Statemashine.Delete_Condition( path.split("/")[-2], path.split("/")[-1], name)
+
+
+	def Go_To(self):
+		self.Statemashine.GoToState( self.SelectedState )
