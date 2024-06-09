d04b13a861ecc587682517ceffc8f93e0f50805d
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-06-09 23:17:08 +0200

    Proper setup.

---
+++ modules/suspects/project1/bananaMash/extBananaMash.py
@@ -0,0 +1,120 @@
+'''Info Header Start
+Name : extBananaMash
+Author : Wieland@AMB-ZEPH15
+Saveorigin : bananaMash.toe
+Saveversion : 2022.32660
+Info Header End'''
+
+class extBananaMash:
+	"""
+	extBananaMash description
+	"""
+	def __init__(self, ownerComp):
+		# The component to which this extension is attached
+		self.ownerComp = ownerComp
+		self.ownerComp.op("eventEmitter").Attach_Emitter(self)
+		self.GoTo( self.ownerComp.par.Initstate.eval() )
+
+	def _publicateEvent(self, event, *args):
+		self.ownerComp.op("callbackManager").Do_Callback(
+			f"on{event}", *(args + (self.Datastore, self.ownerComp))
+		)
+		self.Emit(
+			f"on{event}", *(args + (self.Datastore, self.ownerComp))
+		)
+		return
+	
+
+	@property
+	def CurrentState(self):
+		return self.ownerComp.par.Currentstate.eval()
+	
+	@property
+	def NextState(self):
+		return self.ownerComp.par.Nextstate.eval()
+
+	@property
+	def _states(self) -> COMP:
+		return self.ownerComp.op("statesRepo").Repo
+
+	@property
+	def Datastore(self) -> COMP:
+		return self.ownerComp.op("datastoreRepo").Repo
+
+	def _getState(self, stateName:str) -> COMP:
+		returnState = self._states.op( stateName )
+		if returnState is None:
+			raise Exception(f"Invalid State currently active. Could not find State {self.ownerComp.par.Currentstate.eval()}. Re-Init statemashine!")
+		return returnState
+	
+	def _runTimer(self, timerComp, length, activate = False):
+		timerComp.par.length.val 	= length
+		if activate: timerComp.par.active.val 	= True
+		timerComp.par.initialize.pulse()
+		timerComp.par.start.pulse()
+
+	def Check(self, triggerParameter = None):
+		currentStateComp:COMP = self._getState( self.ownerComp.par.Currentstate.eval() )
+	
+		for connectionContainer in currentStateComp.op("_connections").findChildren( depth = 1, type = baseCOMP):
+
+			if triggerParameter and not tdu.match( connectionContainer.par.Parameterfilter.eval(), triggerParameter): 
+				"""The Parameters that triggered the statechange are not in the defined filter for the transition.
+				The means we are skipping this transition."""
+				continue
+
+			if connectionContainer.op("check").module.check(
+				self.Datastore,
+				self.ownerComp
+			): 
+				self._transitionTo( 
+					connectionContainer.par.Target.eval(),
+					connectionContainer.par.Duration.eval() )
+				return True
+		
+		self._publicateEvent(f"StateCycle", self.CurrentState)
+		self._publicateEvent(f"StateCycle__{self.CurrentState}", self.CurrentState)
+		return False
+	
+	def _transitionTo(self, targetState:str, transitionTime:float):
+
+		self.ownerComp.par.Nextstate.val = targetState
+		self.ownerComp.op("stateTimer").par.initialize.pulse()
+
+		self._publicateEvent("StateExit", self.CurrentState, self.NextState, transitionTime )
+		self._publicateEvent(f"StateExit__{self.CurrentState}", self.CurrentState, self.NextState, transitionTime )
+
+		self._publicateEvent("TransitionStart", self.CurrentState, self.NextState, transitionTime )
+		self._publicateEvent(f"TransitionStart__{self.CurrentState}__{self.NextState}", self.CurrentState, self.NextState, transitionTime )
+
+		if transitionTime:
+			self.ownerComp.par.Mode.val = "Transition"
+			self._runTimer( self.ownerComp.op("transitionTimer"), transitionTime, activate=True )
+		else:
+			self._TransitionDone()
+		return
+	
+	def GoTo(self, targetState):
+		self._transitionTo( targetState, 0)
+
+	def _TransitionDone(self, transitionTime = 0):
+		self.ownerComp.op("transitionTimer").par.active.val = False
+		newCurrentState 					= self.ownerComp.par.Nextstate.eval()
+
+		self._publicateEvent("StateEnter", self.NextState, self.CurrentState, transitionTime )
+		self._publicateEvent(f"StateEnter__{newCurrentState}", self.NextState, self.CurrentState, transitionTime )
+		self._publicateEvent("TransitionEnd", self.NextState, self.CurrentState, transitionTime )
+		self._publicateEvent(f"TransitionEnd__{self.CurrentState}__{self.NextState}", self.CurrentState, self.NextState, transitionTime )
+
+
+		self.ownerComp.par.Currentstate.val = newCurrentState
+		self.ownerComp.par.Nextstate.val 	= ""
+		self.ownerComp.par.Mode.val 		= "State"
+
+		newCurrentStateComp = self._getState( newCurrentState )
+		duration = newCurrentStateComp.par.Duration.eval()
+		self._runTimer( self.ownerComp.op("stateTimer"), duration )
+
+		if self.ownerComp.par.Checkonstateenter.eval(): self.Check()
+
+		
